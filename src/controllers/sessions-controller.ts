a2d9cd60e4b19538c12949c454059a509322a5b7
Kevin Solar <kevinleitesolar@hotmail.com>
2025-08-01 18:59:13 -0300

    feat: addd users to database, create sessions controller and route, retreving the User from route.

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
+import { compare } from "bcrypt"
+import { Request, Response } from "express"
+import z from "zod"
+
+class SessionsController {
+	async create(request: Request, response: Response) {
+		const bodySchema = z.object({
+			email: z.string().email(),
+			password: z.string().min(6),
+		})
+
+		const { email, password } = bodySchema.parse(request.body)
+
+		const user = await prisma.user.findFirst({ where: { email } })
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const passwordMatched =  await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+		return response.json({ message: "ok" })
+	}
+}
+
+export { SessionsController }
--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import { hash } from "bcrypt"
 import z from "zod"
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
 
 class UsersController {
 	async create(request: Request, response: Response) {
@@ -14,9 +16,27 @@ class UsersController {
 
 		const { name, email, password } = bodySchema.parse(request.body)
 
+		/* Cria uma verificacao de se o email ja existe no banco de dados */
+		const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+		if (userWithSameEmail) {
+			throw new AppError("User with same email already exists")
+		}
+		/**/
+
 		const hashedPassword = await hash(password, 8)
 
-		return response.json({ meessage: "ok", hashedPassword })
+		const user = await prisma.user.create({
+			data: {
+				name,
+				email,
+				password: hashedPassword,
+			},
+		})
+
+		const { password: _, ...userWithoutPassword } = user
+
+		return response.status(201).json(userWithoutPassword)
 	}
 
 	index(request: Request, response: Response) {
