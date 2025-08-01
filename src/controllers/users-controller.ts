2a7849376b9786b0b0d86cf940122234b8f5866e
Kevin Solar <kevinleitesolar@hotmail.com>
2025-08-01 17:49:21 -0300

    feat: encrypting the password for user's security

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,19 +1,22 @@
 import { Request, Response } from "express"
+import { hash } from "bcrypt"
 import z from "zod"
 
 class UsersController {
-	create(request: Request, response: Response) {
+	async create(request: Request, response: Response) {
 		const bodySchema = z.object({
-      // faz um tratamento de retirar os espacos do comeco/fim e obriga ter pelo menos 1 caracter
+			// faz um tratamento de retirar os espacos do comeco/fim e obriga ter pelo menos 1 caracter
 			name: z.string().trim().min(2),
 			email: z.string().email(),
-      // senha com no minimo 6 caracteres
+			// senha com no minimo 6 caracteres
 			password: z.string().min(6),
 		})
 
 		const { name, email, password } = bodySchema.parse(request.body)
 
-		return response.json({ meessage: "ok" })
+		const hashedPassword = await hash(password, 8)
+
+		return response.json({ meessage: "ok", hashedPassword })
 	}
 
 	index(request: Request, response: Response) {
