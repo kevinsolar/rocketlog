fd01f8022a66a0e40cc95c3459800428a07931a1
Kevin Solar <kevinleitesolar@hotmail.com>
2025-08-01 17:40:51 -0300

    feat: validating user data before send to DB with Zod

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express"
+import z from "zod"
 
 class UsersController {
 	create(request: Request, response: Response) {
+		const bodySchema = z.object({
+      // faz um tratamento de retirar os espacos do comeco/fim e obriga ter pelo menos 1 caracter
+			name: z.string().trim().min(2),
+			email: z.string().email(),
+      // senha com no minimo 6 caracteres
+			password: z.string().min(6),
+		})
+
+		const { name, email, password } = bodySchema.parse(request.body)
+
 		return response.json({ meessage: "ok" })
 	}
 
