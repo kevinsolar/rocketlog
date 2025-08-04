b62ff0eeed1accc5fac907c28059af4ee9341e5f
Kevin Solar <kevinleitesolar@hotmail.com>
2025-08-04 18:19:45 -0300

    feat: add show costumer name and update/patch the status of delivery

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,4 +1,5 @@
 import { DeliveriesController } from "@/controllers/deliveries-controller"
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller"
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
 import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization"
 import { Router } from "express"
@@ -6,10 +7,13 @@ import { Router } from "express"
 const deliveriesRoutes = Router()
 
 const deliveriesController = new DeliveriesController()
+const deliveriesStatusController = new DeliveriesStatusController()
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]))
 
 deliveriesRoutes.post("/", deliveriesController.create)
 deliveriesRoutes.get("/", deliveriesController.index)
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update)
 
 export { deliveriesRoutes }
