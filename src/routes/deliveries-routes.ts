a80805f5527597b60ca2f9ab301f9f8450e98301
Kevin Solar <kevinleitesolar@hotmail.com>
2025-08-04 16:47:01 -0300

    feat: create the auth validation and verify what the tipo of autorization, create the delivery and list all the packages.

---
+++ src/routes/deliveries-routes.ts
@@ -0,0 +1,15 @@
+import { DeliveriesController } from "@/controllers/deliveries-controller"
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization"
+import { Router } from "express"
+
+const deliveriesRoutes = Router()
+
+const deliveriesController = new DeliveriesController()
+
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]))
+
+deliveriesRoutes.post("/", deliveriesController.create)
+deliveriesRoutes.get("/", deliveriesController.index)
+
+export { deliveriesRoutes }
--- src/routes/sessions-routes.ts
+++ src/routes/sessions-routes.ts
@@ -2,6 +2,7 @@ import { SessionsController } from "@/controllers/sessions-controller"
 import { Router } from "express"
 
 const sessionsRoutes = Router()
+
 const sessionsController = new SessionsController()
 
 sessionsRoutes.post("/", sessionsController.create)
