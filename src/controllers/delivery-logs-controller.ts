bd0657b50cea6c073bdd9253d8e4819a76be6a91
Kevin Solar <kevinleitesolar@hotmail.com>
2025-08-12 18:34:11 -0300

    fix: update some packages, change some depracated features

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -6,7 +6,7 @@ import z from "zod"
 class DeliveryLogsController {
 	async create(request: Request, response: Response) {
 		const bodySchema = z.object({
-			delivery_id: z.string().uuid(),
+			delivery_id: z.uuid(),
 			description: z.string(),
 		})
 
@@ -40,7 +40,7 @@ class DeliveryLogsController {
 
 	async show(request: Request, response: Response) {
 		const paramsSchema = z.object({
-			delivery_id: z.string().uuid(),
+			delivery_id: z.uuid(),
 		})
 
 		// faz a validacao, recupreando o devlivery_id
